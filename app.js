56f1d0f14c920261375ac7b5afd0b2b0330fb7b5
deepak gupt <dg476504@gmail.com>
2024-09-05 00:07:07 +0530

    app .js file change

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cluster = require('cluster');
-const os = require('os');
 const { mongoose } = require('./config/db');
 const { queryUser } = require('./src/Routes/Userquery');
 const { contactRouter } = require('./src/Routes/contactDetail');
@@ -41,23 +39,7 @@ app.get('/api/protected', authenticateToken, (req, res) => {
     res.send('This is a protected route. User is authenticated.');
 });
 
-// Cluster setup
-if (cluster.isMaster) {
-    // Master process creates worker processes equal to the number of CPU cores
-    const numCPUs = os.cpus().length;
-    console.log(`Master process is running. Forking ${numCPUs} workers...`);
-
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
-        cluster.fork(); // Fork a new worker when one dies
-    });
-} else {
-    // Workers can share the same server port
-    app.listen(port, () => {
-        console.log(`Worker ${process.pid} is running on http://localhost:${port}`);
-    });
-}
+// Server start
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
